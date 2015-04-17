f0f93641f0e9660b055f65e9eeea748e6ca738a3
Tuan Tu <nhatbang@gmail.com>
2015-04-17 08:15:48 +0700

    Add js and php

--- app/templates/admin/footer.php
+++ app/templates/admin/footer.php
@@ -1,4 +1,15 @@
-<?php \helpers\assets::js(helpers\url::admin_template_path().'jquery.js'); ?>
+<!-- End div container-fluid -->
+</div>
+<!-- End page-wrapper -->
+</div>
+<!-- End Page Wrapper -->
+</div>
+<?php 
+\helpers\assets::js(array(
+	helpers\url::admin_template_path().'jquery.js'),
+	helpers\url::admin_template_path().'bootstrap.min.js')
+); ?>
 <?php echo $data['js']; ?>
+
 </body>
 </html>
